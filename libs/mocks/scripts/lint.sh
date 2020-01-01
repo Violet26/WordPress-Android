c46873721d5e1e76ca377085cd21bf4060ce4cd9
Jeremy Massel <jeremy.massel@automattic.com>
2020-01-01 07:49:10 -0700

    Sync mocks

--- libs/mocks/scripts/lint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -eu
-MOCKS_ROOT=$(dirname "$( cd "$(dirname "$0")" ; pwd -P )")
-find "$MOCKS_ROOT/WordPressMocks/src/main/assets" -name "*.json" -type f -exec bundle exec jsonlint {} \;
-echo "Done"
